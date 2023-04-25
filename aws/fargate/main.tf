f6b0f9cc3d5bd1e213b968aab2441a019da412ba
veereshsy05 <126865802+veereshsy05@users.noreply.github.com>
2023-04-25 15:52:06 +0530

    Update main.tf

--- aws/fargate/main.tf
+++ aws/fargate/main.tf
@@ -54,8 +54,8 @@ resource "aws_iam_role_policy_attachment" "task_s3" {
 
 resource "aws_ecs_task_definition" "definition" {
   family                   = "task_definition_name"
-  task_role_arn            = "${var.ecs_task_role}"
-  execution_role_arn       = "${var.ecs_task_execution_role}"
+ # task_role_arn            = "${var.ecs_task_role}"
+ # execution_role_arn       = "${var.ecs_task_execution_role}"
   network_mode             = "awsvpc"
   cpu                      = "256"
   memory                   = "1024"
